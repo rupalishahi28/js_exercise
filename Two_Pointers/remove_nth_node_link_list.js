1e067da73bf9e0dd1073502541f1d4d3eff32e95
rupaliShahi <rupali_928@outlook.com>
2023-12-22 08:57:05 +0000

    added func to remove last nth node from LL

---
+++ Two_Pointers/remove_nth_node_link_list.js
@@ -0,0 +1,34 @@
+const removeNthLastNode = (head, n) => {
+    let right = head;
+    let left = head;
+
+    // move right pointer n element away from the left pointer
+    for(let i = 0; i < n; i++) {
+        right = right.next;
+    }
+
+    // Removal of head node
+    if(!right) {
+        return head.next;
+    }
+
+    // move both pointers until right pointer reaches the last node
+    while(right.next !== null) {
+        right = right.next;
+        left = left.next;
+    }
+
+    // At this point left pointer points to (n-1)th element
+    // So link it to next to next element of left
+    left.next = left.next.next;``
+
+    return head;
+}
+
+/** TIME COMPLEXITY
+ * The time complexity is O(n), where n is the number of nodes in linked list
+ */
+
+/** SPACE COMPLEXITY
+ * The space complexity is O(1), because we use constant space to store two pointers.
+ */
